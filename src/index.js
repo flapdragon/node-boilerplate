86f6c531132ffb15ec093b68b92284b88881d75a
David Dean <vermicious.knid.d@gmail.com>
2017-12-07 16:21:23 -0700

    Add dependencies and devdependencies. Add basic express hello world.

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+
+const app = express()
+
+// 3rd party middleware
+app.use(cors({
+	exposedHeaders: [ "Link" ]
+}))
+
+// logger
+app.use(morgan('dev'))
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+export default app;
